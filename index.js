1312e3f5fcebb75c19eb6590bd406e1e47fcddbc
Eugene Dementjev <daemos@x-cart.com>
2017-10-26 14:41:43 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+var express = require('express')
+var path = require('path');
+var Cacheman = require('cacheman')
+var FileCache = require('cacheman-file')
+var sortedObject = require('sorted-object')
+var hash = require('object-hash');
+var google = require('googleapis')
+var search = google.customsearch({
+  version: 'v1',
+  params: { auth: 'AIzaSyAJb0k9_k-7BBsH5zokCAmzFi6b_q7mM8Q', cx: '008796395008737420327:78zys-vxswo' }
+});
+
+var app = express()
+var cache = new Cacheman('search', {
+    ttl: 86400,
+    engine: new FileCache({
+        tmpDir: path.join(process.cwd(), 'cache')
+    })
+})
+
+const CACHE_STATUS_HEADER = 'X-Cache-Status';
+
+var cacheKey = function(req) {
+    return hash(sortedObject(req.query))
+}
+
+var cacheMiddleware = function(req, res, next) {
+    if (!req.query.q) {
+        res.status(400).end();
+        return console.log('No query specified');
+    }
+
+    cache.get(cacheKey(req), (err, value) => {
+        if (err || value === null) {
+            next()
+        } else {
+            res.set(CACHE_STATUS_HEADER, 'Hit').send(value)  
+        }  
+    })
+}
+
+var proxyMiddleware = function(req, res, next) {    
+    res.set(CACHE_STATUS_HEADER, 'Miss');
+
+    var query = req.query;
+    if (!query.siteSearch) {
+        query.q += ' site:kb.x-cart.com OR site:devs.x-cart.com';        
+    }
+
+    console.log(query);
+
+    search.cse.list(query, (err, search) => {
+        cache.set(cacheKey(req), search);
+        res.send(search);
+    })
+}
+
+var handler = [cacheMiddleware, proxyMiddleware];
+
+app.use('/search', handler)
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Google Search caching proxy has started successfully')
+})
